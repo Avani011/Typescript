5a5559b2274eaef6f06274acd4d9afbda3c17bd6
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-06-18 05:16:11 +0530

    Classes in TS
    Here we learnt how to initialise class in typescript and what differences occur when we are calling the constructor or creating an object of the class.

--- 05Project/dist/index.js
+++ 05Project/dist/index.js
@@ -1,3 +1,15 @@
 "use strict";
-console.log("Typescript is stil here");
-console.log("Typescript is amazing");
+class User {
+    /* By providing the empty string to city we avoid the error line and
+    there is no need to intialize it in the constructor */
+    constructor(email, name) {
+        // this two parmeters show that the email and name are string
+        this.city = "";
+        this.email = email;
+        this.name = name;
+    }
+}
+const avani = new User("a@a.com", "avani");
+// in constructor we don't have to write name: "avani"
+avani.city = "jaipur";
+// here we cannot add any other value than string
--- 05Project/src/index.ts
+++ 05Project/src/index.ts
@@ -1,3 +1,18 @@
-console.log("Typescript is stil here");
+class User {
+    email: string
+    name: string
+    // this two parmeters show that the email and name are string
+    city: string = ""
+    /* By providing the empty string to city we avoid the error line and
+    there is no need to intialize it in the constructor */ 
+    constructor(email: string, name : string){
+        this.email = email;
+        this.name = name;
+    }
+}
 
-console.log("Typescript is amazing");
+const avani = new User("a@a.com", "avani");
+// in constructor we don't have to write name: "avani"
+
+avani.city = "jaipur"
+// here we cannot add any other value than string
