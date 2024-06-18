6b88e5f18dfb7a2307386c23ed14aa0fbae226e5
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-06-18 06:13:44 +0530

    More about Interface in TS
    Here we got to know how a class can inherit the interface and what are some rules for inheritance.

---
+++ 05Project/dist/second.js
@@ -0,0 +1,22 @@
+"use strict";
+class Instagram {
+    constructor(cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+}
+/* from this we can say that interface is something that restrict
+us to have the variables define inside it must be present in the
+class which is inheriting it */
+class YouTube {
+    constructor(cameraMode, filter, burst, short) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+        this.short = short;
+    }
+    createStory() {
+        console.log("Story Created");
+    }
+}
---
+++ 05Project/src/second.ts
@@ -0,0 +1,34 @@
+interface TakePhoto{
+    cameraMode: string
+    filter: string
+    burst: number
+}
+
+interface Story {
+    createStory(): void
+}
+
+class Instagram implements TakePhoto{
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number
+    ){}
+}
+
+/* from this we can say that interface is something that restrict
+us to have the variables define inside it must be present in the
+class which is inheriting it */
+
+class YouTube implements TakePhoto, Story{
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number,
+        public short: number
+    ){}
+
+    createStory(): void {
+        console.log("Story Created");
+    }
+}
