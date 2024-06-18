5461bddc1e8a5d33c0a5f68ffdce1c6b9ea426c4
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-06-18 06:01:04 +0530

    Access Modifiers, Getter & Setter
    Here we learn about the 3 access modifiers we use in typescript and how they work. Also, we learned about getter and setter functions and the rules to define them.

--- 05Project/dist/index.js
+++ 05Project/dist/index.js
@@ -4,12 +4,50 @@ class User {
     there is no need to intialize it in the constructor */
     constructor(email, name) {
         // this two parmeters show that the email and name are string
-        this.city = "";
+        this.city = "Jaipur";
         this.email = email;
         this.name = name;
     }
 }
 const avani = new User("a@a.com", "avani");
 // in constructor we don't have to write name: "avani"
-avani.city = "jaipur";
+// avani.city = "jaipur"
 // here we cannot add any other value than string
+//avani.name this will give error line as it is in private mode
+//# is used in javascript and here in typescript we have private keyword public
+// in projects we can also wite class in this way:
+//GETTER AND SETTER:
+class Usern {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "Jaipur";
+    }
+    deleteToken() {
+        console.log("token Deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+const kartikey = new Usern("k@g.com", "Kartikey");
+class SubUser extends Usern {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4; //here as it is private we are not able to acess it
+        //if we mark it protected it can be accessible in this class and the class that inherit it
+    }
+}
--- 05Project/src/index.ts
+++ 05Project/src/index.ts
@@ -1,8 +1,8 @@
 class User {
-    email: string
-    name: string
+    public email: string
+    private name: string
     // this two parmeters show that the email and name are string
-    city: string = ""
+    readonly city: string = "Jaipur"
     /* By providing the empty string to city we avoid the error line and
     there is no need to intialize it in the constructor */ 
     constructor(email: string, name : string){
@@ -14,5 +14,53 @@ class User {
 const avani = new User("a@a.com", "avani");
 // in constructor we don't have to write name: "avani"
 
-avani.city = "jaipur"
+// avani.city = "jaipur"
 // here we cannot add any other value than string
+
+//avani.name this will give error line as it is in private mode
+//# is used in javascript and here in typescript we have private keyword public
+
+// in projects we can also wite class in this way:
+//GETTER AND SETTER:
+class Usern {
+    protected _courseCount = 1
+
+    readonly city: string = "Jaipur"
+    constructor(
+        public email: string,
+        public name: string
+    ){ }
+
+    private deleteToken(){
+        console.log("token Deleted")
+    }
+
+    get getAppleEmail(): string {
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("course count should be more than 1")
+        }
+        this._courseCount = courseNum
+    }
+    //for setter we can't have any return type as it is used to set some value 
+    // we are just simply using get keyword to make function getter and set keyword for function setter
+
+}
+
+const kartikey = new Usern("k@g.com", "Kartikey");
+
+
+class SubUser extends Usern{
+    isFamily: boolean = true
+    changeCourseCount(){
+        this._courseCount = 4 //here as it is private we are not able to acess it
+        //if we mark it protected it can be accessible in this class and the class that inherit it
+    }
+}
